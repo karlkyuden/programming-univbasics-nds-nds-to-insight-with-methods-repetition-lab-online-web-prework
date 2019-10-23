a4a1e188689b5868a0f130abe2df729e3f9aa5f2
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-23 18:37:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,6 +26,15 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  index =0
+  director_list = Array.new
+  
+  while index < source.size do
+    director_list << source[index][:name]
+    index += 1
+  end
+  director_list
+  
 end
 
 def total_gross(source)
@@ -39,6 +48,10 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
   
+  grandTotal = 0
+  director_index = 0
+  
+  while director_index < source.siz
   
 end
 
